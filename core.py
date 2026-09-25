f7eb5d216e8f1eab0bc3ed168ea2913aa6ac669b
SectorCorruptor <mohammed.r210@outlook.com>
2026-09-25 18:50:46 +0400

    should have pushed this before

--- core.py
+++ core.py
@@ -29,7 +29,7 @@ class Core:
         # until the next game.
         self.out_of_eval = False
 
-    def get_move(self, fen, wtime=None, btime=None, winc=None, binc=None, movetime=3000, depth=30):
+    def get_move(self, fen, wtime=None, btime=None, winc=None, binc=None, movetime=3000, depth=37):
         if not self.out_of_eval:
             try:
                 move = _query.query_from_url(fen)
